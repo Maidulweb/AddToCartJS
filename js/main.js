6e4aa9e0007bcad8741f77665b11b9a2af237301
Maidul Islam <34934101+vatia60@users.noreply.github.com>
2020-09-14 13:36:38 +0600

    number added cart header menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 let carts = document.querySelectorAll('.add-to-cart');
 
 for (let i = 0; i < carts.length; i++) {
-   console.log('ok');	
+   carts[i].addEventListener('click', ()=>{
+      cartNumbers();
+     
+   });
 }
+
+
+function onLoadCarts(){
+   let productNumbers = localStorage.getItem('cartNumbersKey');
+   
+   if(productNumbers){
+      document.querySelector('.cart-span').textContent = productNumbers;
+   }
+   
+}
+
+function cartNumbers(){
+   let productNumbers = localStorage.getItem('cartNumbersKey');
+   
+   productNumbers = parseInt(productNumbers);
+
+   if(productNumbers){
+      localStorage.setItem('cartNumbersKey', productNumbers + 1);
+      document.querySelector('.cart-span').textContent = productNumbers + 1;
+   }else {
+      localStorage.setItem('cartNumbersKey', 1);
+      document.querySelector('.cart-span').textContent = 1;
+   }
+   
+}
+
+onLoadCarts();
